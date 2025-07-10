492556986c705dd8010688050709ae6c3b8ad7b8
Allan Chica <ninjalover249@gmail.com>
2025-07-10 11:52:37 -0300

    fix: time input empty

--- app/src/App.vue
+++ app/src/App.vue
@@ -62,7 +62,7 @@ async function searchJobs() {
 		const res = await fetch(`/api/jobs
 			?keywords=${formatedKeywords}
 			&location=${formatedLocation}
-			&timespan=${timespan.value}
+			${timespan.value == '' ? `&timespan=${timespan.value}` : ''}
 			&remote=${isRemote.value}`)
 
 		const data = await res.json()
